3cb405920f1e6170153a2e3e99220fce67182f0b
VullnetJ <v.jata24@gmail.com>
2021-08-30 07:21:56 +0300

    first commit

---
+++ ab.R
@@ -0,0 +1,26 @@
+
+f <- function (a, b) {
+  a^2
+  print(a - b)
+ print(b)
+}
+f(23, 3)
+
+myplot <- function(x, y, type = "1", ...){
+  plot(x, y, type = type, ...)
+}
+args(paste)
+function(..., sep = " ", collapse = NULL)
+  
+lm <-function(x) { x* x}
+lm
+make.NegLogLik <-function(data, fixed = c(FALSE, FALSe){
+  params <-fixed
+  function(p) {param[!fixed] <-p
+  mu <-params[1]
+  sigma <-param[2]
+  a <- 0.5*lenght(data)*long(2*pi*sigma^2) <
+  b
+    
+  }
+}
